9d99e918ecdcde06c3fb5681ffa8b66c9a231638
Bryan Keane <20089166@mail.wit.ie>
2020-11-17 11:01:17 +0000

    Implemented listIterator.java and fixed listIterator in MyList.java

---
+++ src/warehouseApp/listIterator.java
@@ -0,0 +1,22 @@
+package warehouseApp;
+
+import java.util.Iterator;
+
+public class listIterator<K> implements Iterator<K> {
+
+    private Node<K> pos;
+
+    public listIterator(Node<K> node){pos=node;}
+
+    @Override
+    public boolean hasNext() {
+        return pos!=null;
+    }
+
+    @Override
+    public K next() {
+        Node<K> temp = pos;
+        pos = pos.next;
+        return temp.getContents();
+    }
+}
